6df9c8c6ca9e65fa2f889aa726851eafa0a5de17
sepi0l <edykmoney@gmail.com>
2024-04-30 15:54:46 +0300

    added styles and components to view image screen as well as dynamic colors

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, Dimensions, ImageBackground } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
   //console.log(Dimensions.get('screen'));
   return (
-    <WelcomeScreen />
+    <ViewImageScreen />
   );
 }
 
--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import { Image, StyleSheet, View } from 'react-native'
+
+import colors from '../config/colors'
 
 function ViewImageScreen(props) {
     return (
-        <Image source={require('../assets/chair.jpg')}/>
+        <View style={styles.imageContainer}>
+            <View style={styles.closeIcon}></View>
+            <View style={styles.deleteIcon}></View>
+            <Image 
+            source={require('../assets/chair.jpg')} 
+            style={styles.image}
+            resizeMode='contain'
+            />
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: '100%'
+    },
+    imageContainer: {
+        backgroundColor: colors.black,
+        flex: 1
+    },
+    closeIcon: {
+        width: 50,
+        height: 50,
+        backgroundColor: colors.primary,
+        position: 'absolute',
+        top: 50,
+        left: 30
+    },
+    deleteIcon: {
+        width: 50,
+        height: 50,
+        backgroundColor: colors.secondary,
+        position: 'absolute',
+        top: 50,
+        right: 30
+    }
+})
 
 export default ViewImageScreen;
